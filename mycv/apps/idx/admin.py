f1eb619962532cfd1f025f6704093dec2af8484a
Sokirlov <64358905+Sokirlov@users.noreply.github.com>
2020-08-02 12:00:59 +0300

    Add Mobile version in css Add languages tabs in admin zone

--- mycv/apps/idx/admin.py
+++ mycv/apps/idx/admin.py
@@ -12,11 +12,20 @@ admin.site.register(Category, CategoryAdmin)
 class ProductAdmin(TranslationAdmin):
     list_display = ['name', 'idsort', 'category', 'slug', 'startwork', 'endwork']
     list_filter = ('category','slug')
+    fieldsets =(
+        ('Заголовок', {'fields':('category', 'name', ('slug', 'idsort'), ('startwork', 'endwork'),'content',),}),
+        )
     class Media:
         js = (
             '/static/tiny_mce/tiny_mce.js',
             '/static/tiny_mce/tiny_mce_init.js',
+            '/static/modeltranslation/js/force_jquery.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js',
+            '/static/modeltranslation/js/tabbed_translation_fields.js',
         )
+        css = {
+            'screen': ('/static/modeltranslation/css/tabbed_translation_fields.css',),
+        }
 admin.site.register(Block, ProductAdmin)
 
 # @admin.register(Contacts)
